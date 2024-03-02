fa78afb3f898918af94455b6d30f300702e521d4
Crowdbotics <team@crowdbotics.com>
2024-03-02 00:53:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pkequipment-46763",
+  url: "https://pkequipment-46763.botics.co",
+  api: "https://pkequipment-46763.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
